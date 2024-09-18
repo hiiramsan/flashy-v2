4d85b04ca2c7df571111033b71a66bf78b0b2a24
777Styx <jullianpuerta@gmail.com>
2024-09-17 18:58:43 -0700

    errores personales

--- controllers/flashcards.js
+++ controllers/flashcards.js
@@ -1,20 +1,16 @@
 const Flashcard = require("../models/flashcard.js");
 const Card = require("../models/card.js");
-const { flashcardSchema } = require("../validateSchemas.js"); 
+const { flashcardSchema } = require("../validateSchemas.js");
 const ExpressError = require("../utils/ExpressError.js");
-
 
 module.exports.index = async (req, res) => {
   const flashcards = await Flashcard.find({ author: req.user._id })
-    .populate("author")
-  /*   .populate("cards"); */
+    .populate("author");
   if (flashcards.length === 0) {
     req.flash('info', "Create your first set!");
   } else {
     req.flash('info', "Create your second set!");
   }
-
-  /* console.log("Info Flash Message:", req.flash('info'));  */
 
   res.render("flashcards/home", { flashcards });
 };
@@ -22,7 +18,7 @@ module.exports.index = async (req, res) => {
 module.exports.create = async (req, res, next) => {
   try {
     const { error } = flashcardSchema.validate(req.body);
-    if(error) {
+    if (error) {
       const msg = error.details.map(el => el.message).join(',');
       throw new ExpressError(msg, 400);
     }
@@ -39,18 +35,17 @@ module.exports.create = async (req, res, next) => {
     flashcard.cards = cards.map(card => card._id);
     await flashcard.save();
 
-    req.flash('success', "Flashcard deck created successfully!")
+    req.flash('success', "Flashcard deck created successfully!");
     res.redirect(`/flashcards/${flashcard._id}`);
   } catch (error) {
     console.error(error);
-    res.redirect('/flashcards/create')
+    res.redirect('/flashcards/create');
   }
-  
 };
 
 module.exports.renderCreate = (req, res) => {
   res.render('flashcards/create');
-}
+};
 
 module.exports.showFlashcard = async (req, res) => {
   try {
@@ -66,33 +61,20 @@ module.exports.showFlashcard = async (req, res) => {
     req.flash("error", "Something went wrong");
     return res.redirect("/flashcards");
   }
-
-}
+};
 
 module.exports.deleteFlashcard = async (req, res) => {
   const { id } = req.params;
   await Flashcard.findByIdAndDelete(id);
-  res.redirect('/flashcards')
-}
+  res.redirect('/flashcards');
+};
 
-//Seraches for all the Flashcards(Decks) that have
-//the attribute "isVisible" set to true
+// Searches for all the Flashcards(Decks) that have the attribute "isVisible" set to true
 module.exports.explore = async (req, res) => {
   try {
-
-    //Serches the flashcard with the attribute "isVisible" set to true
     const flashcards = await Flashcard.find({ isVisible: true })
       .populate("author");
 
-    //Seraches for all the Flashcards(Decks) that have
-    //the attribute "isVisible" set to true
-    module.exports.explore = async (req, res) => {
-      try {
-      //Serches the flashcard with the attribute "isVisible" set to true
-    const flashcards = await Flashcard.find({ isVisible: true })
-      .populate("author");
-    
-    // If no flashcards are found will send message
     if (flashcards.length === 0) {
       req.flash('info', 'No visible flashcards found');
       return res.redirect('/flashcards');
@@ -100,10 +82,9 @@ module.exports.explore = async (req, res) => {
 
     // Renders found flashcards
     res.render('flashcards/explore', { flashcards });
-
   } catch (error) {
     console.error(error);
     req.flash("error", "Something went wrong");
     res.redirect("/flashcards");
   }
-}
+};
